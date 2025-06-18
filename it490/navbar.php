00dc5c1234cb4b6e0639a0b4f5df79322e476ac9
KennethCardenas <157856640+KennethCardenas@users.noreply.github.com>
2025-06-18 15:53:41 -0400

    Update navbar.php

--- it490/navbar.php
+++ it490/navbar.php
@@ -4,9 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/navbar.css">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <title><?= isset($pageTitle) ? htmlspecialchars($pageTitle) : 'Your App Name' ?></title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <nav class="navbar">
