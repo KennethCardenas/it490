147381c14c901234cd3202a032a9b6b6e04c7c9c
KennethCardenas <157856640+KennethCardenas@users.noreply.github.com>
2025-06-18 15:53:16 -0400

    Update logout.php

--- it490/pages/logout.php
+++ it490/pages/logout.php
@@ -21,10 +21,10 @@ if (isset($_GET['confirmed']) && $_GET['confirmed'] === 'true') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logout | Your Application</title>
+    <title>Logout | Your App Name</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/logout.css">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="logout-container">
