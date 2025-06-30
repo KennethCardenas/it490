8e8d7da9bb39d854790449d8202e98ba4765818e
KennethCardenas <157856640+KennethCardenas@users.noreply.github.com>
2025-06-30 19:28:47 -0400

    Update test_consumer.php

--- it490/consumers/test_consumer.php
+++ it490/consumers/test_consumer.php
@@ -5,9 +5,11 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 $connection = new AMQPStreamConnection('100.87.203.113', 5672, 'kac63', 'Linklinkm1!', '/');
 $channel = $connection->channel();
-$channel->queue_declare('test_queue', false, false, false, false);
 
-echo " [*] TEST NODE waiting for messages...\n";
+// Declare durable queue to match producer
+$channel->queue_declare('test_queue', false, true, false, false);
+
+echo " [*] TEST node waiting for messages from 'test_queue'...\n";
 
 $callback = function ($msg) {
     echo "[TEST NODE] Received: ", $msg->body, "\n";
