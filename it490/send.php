96d763d86ba7536db7b88852d8075694241fb41b
KennethCardenas <157856640+KennethCardenas@users.noreply.github.com>
2025-06-30 18:23:10 -0400

    Update send.php

--- it490/send.php
+++ it490/send.php
@@ -4,24 +4,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-// Update with the IP of your MQ server
 $connection = new AMQPStreamConnection('100.87.203.113', 5672, 'kac63', 'Linklinkm1!');
 $channel = $connection->channel();
 
-// Make sure this matches the queue your consumer listens to
-$queueName = 'test_queue';
+$data = json_encode(['type' => 'test', 'payload' => 'Hello from sender']);
+$msg = new AMQPMessage($data);
 
-$channel->queue_declare($queueName, false, true, false, false);
+$channel->basic_publish($msg, '', 'user_request_queue');
 
-$data = json_encode([
-    'type' => 'test',
-    'message' => 'Hello from send.php'
-]);
-
-$msg = new AMQPMessage($data, ['delivery_mode' => 2]);
-$channel->basic_publish($msg, '', $queueName);
-
-echo "[x] Sent: $data\n";
+echo " [x] Sent message\n";
 
 $channel->close();
 $connection->close();
+?>
