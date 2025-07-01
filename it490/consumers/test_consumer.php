a58be76b8f47c7a2f64c0356b20516af92c661d5
KennethCardenas <157856640+KennethCardenas@users.noreply.github.com>
2025-06-30 20:26:27 -0400

    Update test_consumer.php

--- it490/consumers/test_consumer.php
+++ it490/consumers/test_consumer.php
@@ -5,18 +5,16 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 $connection = new AMQPStreamConnection('100.87.203.113', 5672, 'kac63', 'Linklinkm1!', '/');
 $channel = $connection->channel();
-
-// Replace 'test_queue' with your actual queue name
 $channel->queue_declare('user_request_queue', false, false, false, false);
 
-echo " [*] TEST NODE now listening to 'user_request_queue'...\n";
+echo " [*] test Consumer waiting for messages...\n";
 
 $callback = function ($msg) {
-    echo "[TEST NODE] Received: ", $msg->body, "\n";
+    echo " [x] [test] Received: ", $msg->body, "\n";
+    // You can add routing or auth logic here later
 };
 
 $channel->basic_consume('user_request_queue', '', false, true, false, false, $callback);
 
 while ($channel->is_consuming()) {
-    $channel->wait();
 }
