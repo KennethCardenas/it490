5e8dc1c9dd220fe368a0d8a5bfa6e238901759ad
KennethCardenas <157856640+KennethCardenas@users.noreply.github.com>
2025-07-01 17:46:15 -0400

    Update testing_consumer.php

--- it490/testing_consumer.php
+++ it490/testing_consumer.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../api/connect.php'; // $conn (MySQLi)
+require_once __DIR__ . '/../it490/vendor/autoload.php';
+require_once __DIR__ . '/../it490/api/connect.php'; // $conn (MySQLi)
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
