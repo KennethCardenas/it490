f2ed240598af22bd8a5303f8ad04f335b796f556
KennethCardenas <157856640+KennethCardenas@users.noreply.github.com>
2025-07-13 23:23:43 -0400

    Update promote_db_to_qa.sh

--- qa/promote_db_to_qa.sh
+++ qa/promote_db_to_qa.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
-# Configuration
-DEV_DB_NAME="BARKBUDDY"
-DEV_DB_USER="root"
-DEV_DB_PASS="Linklinkm1!"
-
-QA_DB_USER="root"
-QA_DB_PASS="Linklinkm1!"
-QA_IP="100.91.149.60"
-QA_TMP_PATH="/tmp"
-
-DUMP_FILE="/tmp/${DEV_DB_NAME}_dump.sql"
-
 echo "[*] Dumping database from DEV..."
-mysqldump -u "$DEV_DB_USER" -p"$DEV_DB_PASS" "$DEV_DB_NAME" > "$DUMP_FILE"
+
+mysqldump -h 100.70.204.26 -P 3306 -u BARKBUDDYUSER -p'new_secure_password' BARKBUDDY > dev_dump.sql
+
 if [ $? -ne 0 ]; then
   echo "[!] Error: Failed to dump database."
   exit 1
 fi
 
-echo "[*] Transferring dump to QA..."
-scp "$DUMP_FILE" qa_db@"$QA_IP":"$QA_TMP_PATH"
+echo "[*] Copying dump to QA..."
+
+scp dev_dump.sql qa_db@100.91.149.60:/tmp/dev_dump.sql
+
 if [ $? -ne 0 ]; then
-  echo "[!] Error: Failed to transfer dump to QA."
+  echo "[!] Error: Failed to copy dump to QA."
   exit 1
 fi
 
 echo "[*] Restoring dump on QA..."
-ssh qa_db@"$QA_IP" "mysql -u $QA_DB_USER -p$QA_DB_PASS $DEV_DB_NAME < $QA_TMP_PATH/${DEV_DB_NAME}_dump.sql"
+
+ssh qa_db@100.91.149.60 "mysql -u BARKBUDDYUSER -p'new_secure_password' BARKBUDDY < /tmp/dev_dump.sql"
+
 if [ $? -ne 0 ]; then
-  echo "[!] Error: Failed to restore dump on QA."
+  echo "[!] Error: Failed to import dump on QA."
   exit 1
 fi
 
-echo "[✓] DB promotion from DEV to QA complete."
+echo "[✓] Database promoted from DEV to QA successfully."
