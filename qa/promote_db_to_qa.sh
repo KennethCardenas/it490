15843021be3c32c1b1d4924a448aea67bc80c858
KennethCardenas <157856640+KennethCardenas@users.noreply.github.com>
2025-07-14 18:46:43 -0400

    Update promote_db_to_qa.sh

--- qa/promote_db_to_qa.sh
+++ qa/promote_db_to_qa.sh
@@ -6,13 +6,15 @@ DEV_DB_PORT="3306"
 DEV_DB_USER="BARKBUDDYUSER"
 DEV_DB_PASS="new_secure_password"
 DEV_DB_NAME="BARKBUDDY"
-QA_USER="qa_db"
+
+QA_USER="qa-db"
 QA_IP="100.91.149.60"
+QA_DB_USER="BARKBUDDYUSER"
+QA_DB_PASS="test12"  # QA password
 DUMP_FILE="dev_dump.sql"
 REMOTE_PATH="/tmp/$DUMP_FILE"
 
 echo "[*] Dumping database from DEV ($DEV_DB_HOST)..."
-
 mysqldump -h $DEV_DB_HOST -P $DEV_DB_PORT -u $DEV_DB_USER -p"$DEV_DB_PASS" $DEV_DB_NAME > $DUMP_FILE
 
 if [ $? -ne 0 ]; then
@@ -21,7 +23,6 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "[*] Copying dump to QA ($QA_IP)..."
-
 scp $DUMP_FILE $QA_USER@$QA_IP:$REMOTE_PATH
 
 if [ $? -ne 0 ]; then
@@ -30,8 +31,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "[*] Restoring dump on QA..."
-
-ssh $QA_USER@$QA_IP "mysql -u $DEV_DB_USER -p'$DEV_DB_PASS' $DEV_DB_NAME < $REMOTE_PATH"
+ssh $QA_USER@$QA_IP "mysql -u $QA_DB_USER -p'$QA_DB_PASS' $DEV_DB_NAME < $REMOTE_PATH"
 
 if [ $? -ne 0 ]; then
   echo "[!] Error: Failed to restore database on QA."
@@ -39,3 +39,4 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "[✓] Database promoted from DEV to QA successfully."
+
